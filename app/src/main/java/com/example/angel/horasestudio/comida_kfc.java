2846e2b8f59ffd15de17fc7378086bf9120e22e2
Andrew <anedward96@gmail.com>
2019-01-20 16:49:07 -0500

    andrew_Read_Restaurant2

---
+++ app/src/main/java/com/example/angel/horasestudio/comida_kfc.java
@@ -0,0 +1,64 @@
+package com.example.angel.horasestudio;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.BaseAdapter;
+import android.widget.GridView;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class comida_kfc extends AppCompatActivity {
+GridView gridView;
+    String[] fruitNames = {"Combo 1","Hmaburguesa","Pollo 5 presas "};
+    int[] menuFood_kfc_Images = {R.drawable.kfc_cbx1,R.drawable.kfc_hamburguesa,R.drawable.kfc_pollo};
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_comida_kfc);
+
+        gridView = findViewById(R.id.gridview);
+        CustomAdapter customAdapter = new CustomAdapter();
+        gridView.setAdapter(customAdapter);
+
+        gridView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                //Intent intent = new Intent(getApplicationContext());
+            }
+        });
+    }
+
+    private class CustomAdapter extends BaseAdapter{
+        @Override
+        public int getCount() {
+            return menuFood_kfc_Images.length;
+        }
+
+        @Override
+        public Object getItem(int position) {
+            return null;
+        }
+
+        @Override
+        public long getItemId(int position) {
+            return 0;
+        }
+
+        @Override
+        public View getView(int i, View convertView, ViewGroup parent) {
+
+            View view1 = getLayoutInflater() .inflate(R.layout.menufood_kfc,null);
+            TextView name =view1.findViewById(R.id.fruits);
+            ImageView image = view1.findViewById(R.id.images);
+
+            name.setText(fruitNames[i]);
+            image.setImageResource(menuFood_kfc_Images[i]);
+            return view1;
+        }
+    }
+}
