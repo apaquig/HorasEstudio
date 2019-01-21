57356826e406dbf42d5ef39d59fdf1ef8ad02ec4
horasestudio <angelpaqui36@gmail.com>
2019-01-21 15:43:28 -0500

    Rgistro

--- app/src/main/java/com/example/angel/horasestudio/Registrar_User.java
+++ app/src/main/java/com/example/angel/horasestudio/Registrar_User.java
@@ -63,7 +63,7 @@ public class Registrar_User extends Fragment implements Response.Listener<JSONOb
         return vistaUser;
     }
     private void registrarUsuario() {
-    String url="https://2cab987a.ngrok.io/login/registrar.php?cuentaUsuario=" + txtCorreo.getText().toString() +
+    String url="http://0c82bfc3.ngrok.io/login/registrar.php?cuentaUsuario=" + txtCorreo.getText().toString() +
             "&contrasenia=" + txtPassword.getText().toString()+"&cedula=" + txtCedula.getText().toString()+
             "&nombre=" + txtNombre.getText().toString()+"&telefono=" + txtTelefono.getText().toString()+
             "&direccion=" + txtDireccion.getText().toString();
